d928ffa18872bd398cdf8de47d3899257c2724ab
Yanik Kolomatski <yankolo1234@gmail.com>
2018-02-11 12:07:10 -0500

    Add IImageSerializer

---
+++ ImageManipulation/IImageSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManipulation
+{
+    interface IImageSerializer
+    {
+        String Serialize(Image i);
+        Image Parse(String imageData);
+    }
+}
+}
