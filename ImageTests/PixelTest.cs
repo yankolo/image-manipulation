d491da257c6186ba9bb96e290466e7a85740c2bd
Sammy Chaouki <sammychaouki25@gmail.com>
2018-02-14 17:08:50 -0500

    Add PixelTest

---
+++ ImageTests/PixelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageTests
+{
+    [TestClass]
+    public class PixelTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+
+
+
+
+        }
+        [TestMethod]
+        public void ConstructorTest2()
+        {
+
+
+
+
+
+        }
+    }
+}
