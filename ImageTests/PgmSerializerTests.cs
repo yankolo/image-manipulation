7b3dde70d0622d449c3f5863a2613c90e5cbe8cf
Yanik Kolomatski <yankolo1234@gmail.com>
2018-02-14 22:22:06 -0500

    Pgm Seriazlize_ValidImage_Successful()

--- ImageTests/PgmSerializerTests.cs
+++ ImageTests/PgmSerializerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageManipulation;
 
 namespace ImageTests
 {
@@ -7,8 +8,23 @@ namespace ImageTests
     public class PgmSerializerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Serialize_ValidImage_Successful()
         {
+            Pixel[,] pixels = new Pixel[2, 3];
+            pixels[0, 0] = new Pixel(255);
+            pixels[0, 1] = new Pixel(122);
+            pixels[0, 2] = new Pixel(100);
+            pixels[1, 0] = new Pixel(155);
+            pixels[1, 1] = new Pixel(180);
+            pixels[1, 2] = new Pixel(200);
+            Image image = new Image("This is a test image", 255, pixels);
+            string stringToCompare = "P2" + System.Environment.NewLine + "# This is a test image" + System.Environment.NewLine + "3 2" +
+                System.Environment.NewLine + "255" + System.Environment.NewLine + "255 122 100 155 180 200";
+
+            PgmSerializer serializer = new PgmSerializer();
+            string imageAsString = serializer.Serialize(image);
+
+            Assert.AreEqual(stringToCompare, imageAsString);
         }
     }
 }
